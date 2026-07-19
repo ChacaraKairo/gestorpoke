f3d5e57ffc6a773c3ad1e20b2dccac829b2a6c66
Kairo Chácara <144383548+ChacaraKairo@users.noreply.github.com>
2026-07-19 07:18:19 -0300

    feat: mount inline compatibility and offline image cache

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppV2 } from "./AppV2";
+import { BuildFormCompatibility } from "./BuildFormCompatibility";
 import { CompatibilityOverlay } from "./CompatibilityOverlay";
 import { CompetitiveWorkspaceOverlay } from "./CompetitiveWorkspaceOverlay";
+import { ImageCacheEnhancer } from "./ImageCacheEnhancer";
 import { OperationsOverlay } from "./OperationsOverlay";
 import { PokemonManagerOverlay } from "./PokemonManagerOverlay";
 import { TeamRegulationOverlay } from "./TeamRegulationOverlay";
@@ -15,6 +17,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppV2 />
     <VisualEnhancer />
+    <ImageCacheEnhancer />
+    <BuildFormCompatibility />
     <TypeAnalysisOverlay />
     <TeamRegulationOverlay />
     <CompatibilityOverlay />
