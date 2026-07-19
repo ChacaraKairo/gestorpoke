3727753a9394f0435ee0cb2392969658899400ed
Kairo Chácara <144383548+ChacaraKairo@users.noreply.github.com>
2026-07-18 22:54:48 -0300

    feat: enable global type analysis and visual styling

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppV2 } from "./AppV2";
+import { TypeAnalysisOverlay } from "./TypeAnalysisOverlay";
+import { VisualEnhancer } from "./VisualEnhancer";
 import "./styles.css";
 import "./editor.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppV2 />
+    <VisualEnhancer />
+    <TypeAnalysisOverlay />
   </React.StrictMode>,
 );
